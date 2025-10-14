f841549cf5093c81629b3d1dd38f3c7c1a93542f
Angel Ceja <angel28lex@gmail.com>
2025-10-14 14:07:22 -0600

    second commit

--- article.rb
+++ article.rb
@@ -1,21 +1,19 @@
 class Article
-  attr_accessor :title, :headline, :published_on, :body, :author_name, :author_bio
+  attr_accessor :title, :headline, :published_on, :body, :author
 
-  def initialize(title, headline, published_on, body, author_name, author_bio)
-    @title = title
-    @headline = headline
-    @published_on = published_on
-    @body = body
-    @author_name = author_name
-    @author_bio = author_bio
-  end 
+  def initialize(title: nil, headline: nil, published_on: nil, body: nil, author: nil)
+    @title         = title
+    @headline      = headline
+    @published_on  = published_on
+    @body          = body
+    @author        = author
+  end
 
-  # def display 
-  #  puts "tittle: #{@title}"
-  #  puts "headline: #{@headline}"
-  #  puts "published_on: #{@published_on}"
-  #  puts "body: #{@body}"
-  #  puts "author_name: #{@author_name}"
-  #  puts "author_bio: #{@author_bio}"
-  # end
+  def words_count
+    return 0 if @body.nil? || @body.strip.empty?
+     
+    @body.gsub(/[^a-zA-Z\s]/, '').split.size
+  end
 end
+
+
---
+++ author.rb
@@ -0,0 +1,8 @@
+class Author
+  attr_accessor :name, :bio
+
+  def initialize(name:, bio:)
+    @name = name
+    @bio = bio
+  end
+end
