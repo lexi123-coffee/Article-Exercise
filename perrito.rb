6a89d6c63123b74b4208d9bcbba6725f3cd32a26
Angel Ceja <angel28lex@gmail.com>
2025-11-04 11:55:31 -0600

    Agregue nuevo contenido a perrito.rb

--- perrito.rb
+++ perrito.rb
@@ -1,77 +1,55 @@
 class Perrito
-  attr_accessor :nombre, :edad, :raza, :energy, :is_alive
+  MAX_ENERGY = 100
+  MIN_ENERGY = 0
 
-  def initialize(nombre, edad, raza)
+  def initialize(nombre)
     @nombre = nombre
-    @edad = edad
-    @raza = raza
-    @energy = 100
+    @energy = MAX_ENERGY
     @is_alive = true
-  end
-
-  def energia_positiva?
-    @energy > 0
   end
 
   def run
     return if verificar_estado_inactivo
-
-    @energy -= 20
-    @energy = 0 if @energy < 0
-    mostrar_estado_energia
-    puts "#{@nombre} fue a correr un rato y ahora tiene #{@energy} de energia."
-
+    ajustar_energia(-30)
+    puts "#{@nombre} ya ha corrido mucho y por eso ahora ya tiene #{@energy} de energia."
   end
 
   def eat
     return if verificar_estado_inactivo
-
-    @energy += 20
-    @energy = 100 if @energy > 100
-    mostrar_estado_energia
-    puts "#{@nombre} ya comio y por eso ahora tiene #{@energy} de energia."
+    ajustar_energia(+20)
+    puts "#{@nombre} ya comio y por eso ahora ya tiene #{@energy} de energia."
   end
 
   def sleep
     return if verificar_estado_inactivo
-
-    @energy += 10
-    @energy = 100 if @energy > 100
-    mostrar_estado_energia
-    puts "#{@nombre} ya fue a dormir y por eso ahora tiene #{@energy} de energia."
+    ajustar_energia(+40)
+    puts "#{@nombre} durmio un ratito y por eso ahora tiene #{@energy} de energia."
   end
 
   def trick
     return if verificar_estado_inactivo
-
-    @energy -= 20
-    @energy = 0 if @energy < 0
-    mostrar_estado_energia
-    puts "#{@nombre} estaba haciendo un truco y por eso ahora tiene #{@energy} de energia."
+    ajustar_energia(-20)
+    puts "#{@nombre} hizo un truco, por eso ahora tiene #{@energy} de energia."
   end
 
-  private
+  def ajustar_energia(cambio)
+    return puts "#{@nombre} ya no puede hacer trucos porque ya ha fallecido." unless @is_alive
 
-  def verificar_estado_vivo
-    if @energy <= 0
+    @energy += cambio
+    @energy = [[@energy, MAX_ENERGY].min, MIN_ENERGY].max
+
+    if @energy == MIN_ENERGY
       @is_alive = false
-      puts "#{@nombre} esta ya muy cansado y por eso ya no puede seguir"
+      puts "#{@nombre} se ha quedado sin energia y ha fallecido."
     end
   end
 
   def verificar_estado_inactivo
     unless @is_alive
-      puts "#{@nombre} se ha quedado sin energia y ya no puede hacer mas actividades"
+      puts "#{@nombre} ya no puede hacer nada porque ha fallecido."
       return true
     end
     false
   end
-
-  def mostrar_estado_energia
-    if energia_positiva?
-      puts "#{@nombre} tiene #{@energy} de energia."
-    else
-      puts "#{@nombre} esta ya muy cansado y mejor va a descansar."
-    end
-  end
 end
+
